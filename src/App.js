bfdd1fce3569f473c6031a99758c113b03deb2c5
PedroKielma <peterkielma@gmail.com>
2022-04-16 23:33:59 +0100

    envoriment setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { createClient } from 'urql'
+import { useEffect, useState } from 'react'
 import './App.css';
+
+const APIURL = ""
+
+const query = `
+  query {
+    tokens(
+      first: 5
+      orderBy: tokenID
+      orderDirection:desc
+    ) {
+      id
+      tokenID
+      contentURI
+      metadataURI
+    }
+  }
+  `
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
     </div>
   );
 }
